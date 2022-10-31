0da856cc68b9569b2770c7e92d9bc9973845e85e
Mikhail Oleynikov (@Olmeor) <mn.oleynikov@gmail.com>
2022-10-31 22:19:53 +0700

    feat: add solution for 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+
+const readStream = fs.createReadStream(path.join(__dirname, './text.txt'), 'utf-8');
+
+let data = '';
+
+readStream.on('data', chunk => data += chunk);
+readStream.on('end', () => process.stdout.write(data));
+readStream.on('error', error => console.log('Error', error.message));
